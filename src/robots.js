b38d1258e482d4331ca7a7ad2dc30ae77254e5a0
Thav0 <gustavodnz035@gmail.com>
2020-01-03 13:33:28 -0300

    teste

---
+++ src/robots.js
@@ -0,0 +1,44 @@
+export default [
+  {
+    id: 1,
+    name: 'robot',
+    username: 'bre97t',
+    email: 'fsdfsd@gmail.com'
+  },
+  {
+    id: 2,
+    name: 'gus',
+    username: 'bre87t',
+    email: 'fsdfsd@gmail.com'
+  },
+  {
+    id: 3,
+    name: 'fred',
+    username: 'bret666',
+    email: 'fsdfsd@gmail.com'
+  },
+  {
+    id: 4,
+    name: 'silva',
+    username: 'bret111',
+    email: 'fsdfsd@gmail.com'
+  },
+  {
+    id: 5,
+    name: 'preti',
+    username: 'bret33',
+    email: 'fsd222fsd@gmail.com'
+  },
+  {
+    id: 6,
+    name: 'branco',
+    username: 'bret333',
+    email: 'fsdfsd@gmail.com'
+  },
+  {
+    id: 7,
+    name: 'verde',
+    username: 'bret',
+    email: 'fsdfsd@gmail.com'
+  }
+]
